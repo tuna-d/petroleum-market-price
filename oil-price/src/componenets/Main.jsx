8fd733c3c190901ecfff5659368934f70d7ecedf
tuna-d <tdemirel98@gmail.com>
2024-04-22 12:56:08 +0300

    responsive to mobile

--- oil-price/src/componenets/Main.jsx
+++ oil-price/src/componenets/Main.jsx
@@ -38,100 +38,61 @@ export const Main = () => {
     })
 
     function calculateMarketPrice() {
-        if (inputs.api === 0 || inputs.freight === 0 || inputs.discount === 0 || inputs.fee === 0 || inputs.insurance === 0) {
+        const { api, freight, discount, fee, insurance } = inputs
+        const refAPI = constants.refAPI
+
+        if (api === 0 || freight === 0 || discount === 0 || fee === 0 || insurance === 0) {
             alert("Please enter all required data")
         }
         else {
-            if (inputs.api < constants.refAPI) {
-                setCalcData(prevData => ({
-                    ...prevData,
-                    diff: inputs.api - constants.arabHeavy
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    corrPrice: constants.arabHeavyPrice + (prevData.diff * constants.centDiff)
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    freight: (inputs.freight * inputs.discount) / constants.conversationFactor
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    totFreight: inputs.fee / constants.conversationFactor + prevData.freight
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    totInsurance: (prevData.corrPrice + prevData.totFreight) * inputs.insurance / 100
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    marketPrice: prevData.corrPrice + prevData.totFreight + prevData.totInsurance
-                }))
-            }
-            else {
-                setCalcData(prevData => ({
-                    ...prevData,
-                    diff: inputs.api - constants.arabMedium
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    corrPrice: constants.arabMediumPrice + (prevData.diff * constants.centDiff)
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    freight: (inputs.freight * inputs.discount) / constants.conversationFactor
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    totFreight: inputs.fee / constants.conversationFactor + prevData.freight
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    totInsurance: (prevData.corrPrice + prevData.totFreight) * inputs.insurance / 100
-                }))
-                setCalcData(prevData => ({
-                    ...prevData,
-                    marketPrice: prevData.corrPrice + prevData.totFreight + prevData.totInsurance
-                }))
-            }
+            const price = api < refAPI ? constants.arabHeavyPrice : constants.arabMediumPrice
+            const diff = api - (api < refAPI ? constants.arabHeavy : constants.arabMedium)
+            const correlationPrice = price + (diff * constants.centDiff)
+            const totalFreight = (freight * discount) / constants.conversationFactor + fee / constants.conversationFactor
+            const totalInsurance = (correlationPrice + totalFreight) * insurance / 100
+            setCalcData(prevData => ({
+                ...prevData,
+                marketPrice: correlationPrice + totalFreight + totalInsurance
+            }))
         }
     }
 
+
     return (
         <main>
-            <div className="flex mx-10 my-4">
+            <div className="flex max-md:flex-col  md:mx-10 my-4 mx-2">
                 <section className="w-1/2">
                     <h3 className="text-2xl">Required Data:</h3>
                     <form className="flex flex-col w-96 text-base gap-2 mt-4">
                         <div className="flex justify-between">
                             <label htmlFor="api">Crude Oil API:</label>
-                            <input className="px-1 rounded-lg border border-slate-400" type="number" id="api" name="api" value={inputs.api} onChange={handleChange} />
+                            <input className="max-md:w-1/4 max-md:mr-4 px-1 rounded-lg border border-slate-400" type="number" id="api" name="api" value={inputs.api} onChange={handleChange} />
                         </div>
 
                         <div className="flex justify-between">
                             <label htmlFor="freight">Freight cost ($/tone):</label>
-                            <input className="px-1 rounded-lg border border-slate-400" type="number" id="freight" name="freight" value={inputs.freight} onChange={handleChange} />
+                            <input className="max-md:w-1/4 max-md:mr-4 px-1 rounded-lg border border-slate-400" type="number" id="freight" name="freight" value={inputs.freight} onChange={handleChange} />
                         </div>
 
                         <div className="flex justify-between">
                             <label htmlFor="discount">Discount (%):</label>
-                            <input className="px-1 rounded-lg border border-slate-400" type="number" id="discount" name="discount" value={inputs.discount} onChange={handleChange} />
+                            <input className="max-md:w-1/4 max-md:mr-4 px-1 rounded-lg border border-slate-400" type="number" id="discount" name="discount" value={inputs.discount} onChange={handleChange} />
                         </div>
 
                         <div className="flex justify-between items-center">
                             <label htmlFor="fee">Any transportation fees ($/tone):</label>
-                            <input className="px-1 rounded-lg border border-slate-400 h-[26px]" type="number" id="fee" name="fee" value={inputs.fee} onChange={handleChange} />
+                            <input className="max-md:w-1/4 max-md:mr-4 px-1 rounded-lg border border-slate-400 h-[26px]" type="number" id="fee" name="fee" value={inputs.fee} onChange={handleChange} />
                         </div>
 
                         <div className="flex justify-between">
                             <label htmlFor="insurance">Insurance rate (%):</label>
-                            <input className="px-1 rounded-lg border border-slate-400" type="number" id="insurance" name="insurance" value={inputs.insurance} onChange={handleChange} />
+                            <input className="max-md:w-1/4 max-md:mr-4 px-1 rounded-lg border border-slate-400" type="number" id="insurance" name="insurance" value={inputs.insurance} onChange={handleChange} />
                         </div>
                     </form>
                     <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded active:scale-95" onClick={calculateMarketPrice}>Calculate</button>
                 </section>
 
-                <section>
+                <section className="max-md:mt-4">
                     <h3 className="text-2xl">Solution</h3>
                     <p className="mt-4 text-3xl">Market Price: {calcData.marketPrice}</p>
                 </section>
--- oil-price/src/componenets/Navbar.jsx
+++ oil-price/src/componenets/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Navbar = () => {
     return (
         <header>
-            <nav className="bg-slate-400">
+            <nav className="bg-slate-400 w-full">
                 <h1 className="px-12 py-6 text-3xl text-white font-bold">Petroleum Market Price Calculator</h1>
             </nav>
         </header>
